79e590b8384a76586a1b5bdc9b514ded91f4ad46
travis Lawson <travislawclimb@gmail.com>
2020-01-07 06:27:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,36 @@
 def create_an_empty_array
-  []
+  empty_array = []
 end
 
 def create_an_array
-  ["water", "fire", "earth", "air"]
+  famous_cats = ["lil bub", "grumpy cat", "maru", "bill"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array, element).last)
+  famous_cats = ["lil bub", "grumpy cat", "maru", "bill"]
+ famous_cats << "Nala cat"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  famous_cats.unshift("Felix")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  famous_cats.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  famous_cats.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  famous_cats[0]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  famous_cats[1]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  famous_cats[3]
 end
